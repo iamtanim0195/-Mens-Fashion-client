bd93b1f4e17ffe66c2debd32f3854e90fef30d38
iamtanim0195 <sahadatmd195@gmail.com>
2023-10-20 19:15:58 +0600

    brands data fetch form json file

--- src/Components/Brands/Brands.jsx
+++ src/Components/Brands/Brands.jsx
@@ -1,54 +1,33 @@
-const Brands = () => {
+import { useEffect, useState } from 'react';
+
+function Brands() {
+    const [brandData, setBrandData] = useState(null);
+
+    useEffect(() => {
+        // Use fetch to load the JSON data
+        fetch('/brand.json')
+            .then((response) => response.json())
+            .then((data) => setBrandData(data))
+            .catch((error) => console.error('Error fetching brand data:', error));
+    }, []);
+
     return (
-        <div className="flex p-5 gap-2">
-            <div>
-                <img
-                    className="w-96 h-28"
-                    src={`https://i.ibb.co/q9DZNwm/image.png`}
-                    alt="Nike"
-                />
-                <p className="text-3xl">Nike</p>
-            </div>
-            <div>
-                <img
-                    className="w-96 h-28"
-                    src={`https://i.ibb.co/qN251Pq/image.png`}
-                    alt="Adidas"
-                />
-                <p className="text-3xl">Adidas</p> 
-            </div>
-            <div>
-                <img
-                    className="w-96 h-28"
-                    src={`https://i.ibb.co/sQhhKfL/image.png`}
-                    alt="Gucci"
-                />
-                <p className="text-3xl">Gucci</p> 
-            </div>
-            <div>
-                <img
-                    className="w-96 h-28"
-                    src={`https://i.ibb.co/yRSK6Pj/image.png`}
-                    alt="Zara" />
-                <p className="text-3xl">Zara</p> 
-            </div>
-            <div>
-                <img
-                    className="w-96 h-28"
-                    src={`https://i.ibb.co/mb7xPhS/image.png`}
-                    alt="H&M"
-                />
-                <p className="text-3xl">H&M</p>
-            </div>
-            <div>
-                <img
-                    className="w-96 h-28"
-                    src={`https://i.ibb.co/qsf7yZj/image.png`}
-                    alt="Levi's" />
-                <p className="text-3xl">Levi&#39;s</p>
-            </div>
+        <div >
+            {brandData ? (
+                <div className='flex p-5 gap-3'>
+                    {brandData.map((brand, index) => (
+                        <div key={index} >
+                            <img className="w-96 h-28" src={brand.brandImage} alt={brand.brandName} />
+                            <h3 className="text-3xl">{brand.brandName}</h3>
+                        </div>
+                    ))}
+                </div>
+            ) : (
+                <p>Loading brand data...</p>
+            )}
+            {/* Render your component content here */}
         </div>
     );
-};
+}
 
 export default Brands;
