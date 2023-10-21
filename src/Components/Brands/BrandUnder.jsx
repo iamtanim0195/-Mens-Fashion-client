41be1c83916e6c3693550492d0380d291a3236cd
iamtanim0195 <sahadatmd195@gmail.com>
2023-10-21 22:31:10 +0600

    slider done

--- src/Components/Brands/BrandUnder.jsx
+++ src/Components/Brands/BrandUnder.jsx
@@ -1,4 +1,5 @@
 import { useLoaderData, useParams } from "react-router-dom";
+import Slider from "./Slider";
 const BrandUnder = () => {
     const brands = useLoaderData();
     const { brandName } = useParams();
@@ -7,18 +8,24 @@ const BrandUnder = () => {
     console.log(matchingBrands);
     return (
         <div>
-            {matchingBrands.length}
-            <ul>
-                {matchingBrands.map((brand, index) => (
-                    <li key={index}>
-                        <h3>{brand.brandName}</h3>
-                        <p>Category: {brand.category}</p>
-                        <p>Price: {brand.price}</p>
-                        {/* Add more information as needed */}
-                    </li>
-                ))}
-            </ul>
-
+            <Slider/>
+            {matchingBrands.length === 0 ? (
+                <p className="text-4xl text-red-500 text-center">No brands matching {brandName} found</p>
+            ) : (
+                <div>
+                    <h2>Brands matching {brandName}</h2>
+                    <ul>
+                        {matchingBrands.map((brand, index) => (
+                            <li key={index}>
+                                <h3>{brand.brandName}</h3>
+                                <p>Category: {brand.category}</p>
+                                <p>Price: {brand.price}</p>
+                                {/* Add more information as needed */}
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            )}
         </div>
     )
 }
---
+++ src/Components/Brands/Slider.jsx
@@ -0,0 +1,32 @@
+
+const Slider = () => {
+    return (
+        <div>
+            <div className="carousel w-full h-[60vh]">
+                <div id="slide1" className="carousel-item relative w-full">
+                    <img src="https://i.ibb.co/M1cLHsh/3674498.jpg" className="w-full" />
+                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                        <a href="#slide4" className="btn btn-circle">❮</a>
+                        <a href="#slide2" className="btn btn-circle">❯</a>
+                    </div>
+                </div>
+                <div id="slide2" className="carousel-item relative w-full">
+                    <img src="https://i.ibb.co/R3LN2xF/5014631.jpg" className="w-full" />
+                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                        <a href="#slide1" className="btn btn-circle">❮</a>
+                        <a href="#slide3" className="btn btn-circle">❯</a>
+                    </div>
+                </div>
+                <div id="slide3" className="carousel-item relative w-full">
+                    <img src="https://i.ibb.co/x22Hgd3/5501239.jpg" className="w-full" />
+                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                        <a href="#slide3" className="btn btn-circle">❮</a>
+                        <a href="#slide1" className="btn btn-circle">❯</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Slider;
