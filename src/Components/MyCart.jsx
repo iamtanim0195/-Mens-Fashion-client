d201c19a530cbd74997242d64132d62b5cdd7167
iamtanim0195 <sahadatmd195@gmail.com>
2023-10-24 12:55:06 +0600

    delete is workig

--- src/Components/MyCart.jsx
+++ src/Components/MyCart.jsx
@@ -1,8 +1,62 @@
 import { useLoaderData } from "react-router-dom";
+import Swal from "sweetalert2";
 
 const MyCart = () => {
     // Load the cart data using useLoaderData
     const carts = useLoaderData();
+
+    const handleDelete = (_id) => {
+        console.log(_id);
+        Swal.fire({
+            title: 'Are you sure?',
+            text: "You won't be able to revert this!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, delete it!'
+        }).then((result) => {
+            if (result.isConfirmed) {
+
+
+                fetch(`http://localhost:5000/cart/${_id}`, {
+                    method: 'DELETE',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    }
+                })
+                    .then(res => {
+                        if (!res.ok) {
+                            throw new Error(`DELETE request failed with status ${res.status}`);
+                        }
+                        return res.json();
+                    })
+                    .then(data => {
+                        console.log(data);
+                        if (data.deletedCount > 0) {
+                            Swal.fire(
+                                'Deleted!',
+                                'Your product has been deleted.',
+                                'success',
+                            ).then(() => {
+                                // Reload the page after the user confirms the deletion
+                                location.reload();
+                            });
+
+                        }
+
+                    })
+                    .catch(error => {
+                        console.error('Error:', error);
+                        // Handle the error as needed
+                    });
+
+            }
+
+        })
+
+    }
+
 
     // Check if the cart data is not available or not an array
     if (!carts || !Array.isArray(carts) || carts.length === 0) {
@@ -23,7 +77,7 @@ const MyCart = () => {
                             <h2 className="card-title">price: {brand.price}</h2>
                             <h2 className="card-title">rating: {brand.rating}</h2>
                             <div className="card-actions">
-                                <button className="btn btn-primary">Delete product</button>
+                                <button onClick={() => handleDelete(brand._id)} className="btn bg-red-600 text-white">Delete product</button>
                             </div>
                         </div>
                     </div>
